d86512464fdaa7decde2e3e98dbbcdc179615140
Zach Zager <zachzager@gmail.com>
2016-03-31 20:44:10 -0400

    added stylesheet. finished up input methods. put NOTE on index.html

--- elevator.js
+++ elevator.js
@@ -5,138 +5,67 @@ var position;
 var problem;
 var displayStateNumber = 0;
 var stateNumber = 0;
-var totalStates = 5;
 elevatorStateList = [];
 starting_elevator = '';
-
 final_destination = {};
 final_destination.floor = 0;
 final_destination.time = 0;
-
 solutionList = [];
 
-// Elevator States
-
-elevatorState1 = [['.','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','B','.','.'],
-				 ['.','.','C','.'],
-				 ['A','.','.','D']];
-
-elevatorState2 = [['.','.','.','.'],
-				 ['A','B','.','.'],
-				 ['.','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','.','C','D']];
-
-elevatorState3 = [['A','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','B','C','.'],
-				 ['.','.','.','D']];
-
-elevatorState4 = [['A','.','','.'],
-				 ['.','.','C','.'],
-				 ['.','B','.','.'],
-				 ['.','.','.','.'],
-				 ['.','.','.','D']];
-
-elevatorState5 = [['A','.','C','D'],
-				 ['.','.','.','.'],
-				 ['.','B','.','.'],
-				 ['.','.','','.'],
-				 ['.','.','.','.']];
-
-// adjacency check
-/*
-elevatorState1 = [['.','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','B','.','.'],
-				 ['.','.','C','D'],
-				 ['A','.','.','.']];
-
-elevatorState2 = [['.','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','.','.','.'],
-				 ['A','B','C','.'],
-				 ['.','.','.','D']];
-
-elevatorState3 = [['A','.','.','.'],
-				 ['.','.','.','.'],
-				 ['.','B','.','.'],
-				 ['.','.','C','D'],
-				 ['.','.','.','.']];
-
-elevatorState4 = [['A','.','','.'],
-				 ['.','.','C','.'],
-				 ['.','B','.','.'],
-				 ['.','.','.','.'],
-				 ['.','.','.','D']];
-
-elevatorState5 = [['A','.','C','D'],
-				 ['.','.','.','.'],
-				 ['.','B','.','.'],
-				 ['.','.','','.'],
-				 ['.','.','.','.']];
-*/
-
+// get all HTML elements
 function setup() {
-
-	// get all HTML elements
 	form = document.getElementById('file_form');
-	setup_form();
+	setup_form(); // gives the form the submit function
 	fileSelect = document.getElementById('file_select');
-	uploadButton = document.getElementById('upload_button');
-	
 	problem = document.getElementById("problem");
 	solution = document.getElementById("solution");
 	start = document.getElementById("start");
 	fd_floor = document.getElementById("fd_floor");
 	fd_time = document.getElementById("fd_time");
-
-	// retrieve and show elevator states
-	getElevatorStates();
-	printElevatorState(elevatorStateList[displayStateNumber]);
 }
 
 function setup_form() {
 	form.onsubmit = function(event) {
 		event.preventDefault();
-		uploadButton.innerHTML = 'Uploading...'; // Update button text.
-		var files = fileSelect.files; // The rest of the code will go here...
-		console.log(files);
+		elevatorStateList = []; // resets elevatorStateList
+		displayStateNumber = 0; // resets displayStateNumber
+		var files = fileSelect.files;
 		var formData = new FormData();
 
-		// Loop through each of the selected files.
-		for (var i = 0; i < files.length; i++) {
-			var file = files[i];
+		theText = files[0];
+		console.log(theText);
 
-			// Check the file type.
-			if (!file.type.match('image.*')) {
-				continue;
-			}
-
-			// Add the file to the request.
-			formData.append('photos[]', file, file.name);
+		reader = new FileReader();
+		reader.onload = function(e) {
+			parse_text();
+			printElevatorState(elevatorStateList[displayStateNumber]);
 		}
+		reader.readAsText(theText);
+	}
+}
 
-		formData.append(name, file); // Files
-		//formData.append(name, value);  // Strings
+// loops through text placing each elevator state
+// in the elevatorStateList
+function parse_text() {
+	var text = reader.result;
+	var single_state = [];
+	var single_row = [];
 
-		var xhr = new XMLHttpRequest(); // Set up the request.
-
-		// Open the connection.
-		xhr.open('POST', 'handler.php', true);
-
-		// Set up a handler for when the request finishes.
-		xhr.onload = function () {
-			if (xhr.status === 200) {
-				// File(s) uploaded.
-				uploadButton.innerHTML = 'Upload';
-			} else {
-			alert('An error occurred!');
-			}
-		};
+	for (var x = 0; x < text.length; x++) {
+		tmp = Number(x) + 1;
+		console.log(x + " "+ text[x]);
+		if (text[x] == '\n' && text[x + 1] == '\n') {
+			single_state.push(single_row);
+			elevatorStateList.push(single_state);
+			single_state = [];
+			single_row = [];
+			x++;
+		} else if (text[x] == '\n')  { 
+			single_state.push(single_row);
+			single_row = [];
+		}else {
+			single_row.push(text[x]);
+		}
 	}
 }
 
@@ -146,10 +75,8 @@ function calculate() {
 	// sets inputted values
 	final_destination.time = Number(fd_time.value);
 	final_destination.floor = elevatorStateList[0].length - Number(fd_floor.value);
-	starting_elevator = start.value;
-
+	starting_elevator = start.value; // 
 	determinePaths(getFirstCoords(),'',0); // starts checking all states/paths
-
 	printSolution(); // displays solution
 }
 
@@ -161,8 +88,7 @@ function clearInput() {
 
 // searches through all possible paths to find elevator traversal solution
 function determinePaths(curr_coords, path, state, prev_elevator) {
-
-	if (state < totalStates) {
+	if (state < elevatorStateList.length) {
 
 		// check adjacent elevators
 		goToRightElevator(curr_coords, path, state, prev_elevator);
@@ -202,7 +128,6 @@ function goToLeftElevator(curr_coords, path, state, prev_elevator) {
 // adds elevator strings to the solution list if they are on the specified
 // vertical level and the elevators are at the specified state
 function checkForSolution(level, state, path) {
-
 	if (level===final_destination.floor && state===final_destination.time) {
 		solutionList.push(path);
 	}
@@ -210,7 +135,6 @@ function checkForSolution(level, state, path) {
 
 // displays the solution if there is one
 function printSolution(){
-
 	solution.innerHTML = "Solution: ";
 
 	if (solutionList.length > 0) {
@@ -222,12 +146,9 @@ function printSolution(){
 
 // goes through whole elevator state to find starting elevator
 function getFirstCoords() {
-
-	elevatorState = elevatorStateList[0];
-
-	for (x in elevatorState) {
-		for (y in elevatorState[x]) {
-			if (elevatorState[x][y] === starting_elevator) {
+	for (x in elevatorStateList[0]) {
+		for (y in elevatorStateList[0][x]) {
+			if (elevatorStateList[0][x][y] === starting_elevator) {
 				return [Number(x),Number(y)];
 			}
 		}
@@ -240,7 +161,6 @@ function getFirstCoords() {
 //			            the current or next elevator
 // returns: level/height of the specified elevator
 function getLevel(shaft,state) {
-
 	var elevatorState = elevatorStateList[state];
 
 	for (x in elevatorState) {
@@ -252,51 +172,36 @@ function getLevel(shaft,state) {
 
 // responds to user clicking the button to cycle through elevator
 function nextState() {
-
 	displayStateNumber++;
 
-	if (displayStateNumber == totalStates) {
+	if (displayStateNumber == elevatorStateList.length) {
 		displayStateNumber = 0;
 	}
-
 	printElevatorState(elevatorStateList[displayStateNumber]);
 }
 
 // responds to user clicking the button to cycle through elevator
 function prevState() {
-
 	displayStateNumber--;
 
 	if (displayStateNumber < 0) {
-		displayStateNumber = totalStates-1;
+		displayStateNumber = elevatorStateList.length-1;
 	}
-
 	printElevatorState(elevatorStateList[displayStateNumber]);
 }
 
-// stores elevator states in a list
-function getElevatorStates() {
-	elevatorStateList.push(elevatorState1);
-	elevatorStateList.push(elevatorState2);
-	elevatorStateList.push(elevatorState3);
-	elevatorStateList.push(elevatorState4);
-	elevatorStateList.push(elevatorState5);
-}
-
-// displays elevator states
+// displays elevator states, places x's between each letter
 function printElevatorState(elevator) {
 
 	problem.innerHTML = "";
 
-	// draws the board
-	for (i in elevator) {
+	for (i in elevator) { // draws the board
 		problem.innerHTML += "xx";
 		for (j in elevator[i]) {
-			problem.innerHTML += elevator[i][j] + "x";
+			problem.innerHTML += elevator[i][j] + "x"; 
 		}
 		problem.innerHTML += "x<br>";
 	}
-
 	var displayState = displayStateNumber+1;
 	problem.innerHTML += "State: " + displayState;
 }
